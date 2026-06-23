473ddf876d31ba5be62620bf5f4fc65732fc37c6
Sherif-Moheep <sherifmohep@gmail.com>
2026-06-23 23:25:24 +0300

    Update Content.kt

--- app/src/main/java/com/hitech/pickit/media/presentation/models/Content.kt
+++ app/src/main/java/com/hitech/pickit/media/presentation/models/Content.kt
@@ -1,12 +1,18 @@
 package com.hitech.pickit.media.presentation.models
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.LoadingIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.hitech.pickit.media.data.datasource.remote.mappers.toUserMessage
 import com.hitech.pickit.media.presentation.models.base.BaseViewModel
 import com.hitech.pickit.media.presentation.utili.UiState
@@ -14,11 +20,22 @@ import com.hitech.pickit.media.presentation.utili.components.ErrorScreen
 
 @OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
-fun <T> Content(viewModel: BaseViewModel<T>, successScreen: @Composable (T) -> Unit) {
-
+fun <T> Content(
+    viewModel: BaseViewModel<T>,
+    successScreen: @Composable (T) -> Unit
+) {
     when (val state = viewModel.stateFlow.collectAsState().value) {
 
-        is UiState.Loading -> LoadingIndicator(modifier = Modifier.fillMaxSize(),color = MaterialTheme.colorScheme.primaryContainer)
+        is UiState.Loading ->  Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            LoadingIndicator(
+                color = MaterialTheme.colorScheme.primaryContainer,
+                modifier = Modifier.size(250.dp)
+            )
+        }
 
         is UiState.Success -> successScreen(state.data)
 
